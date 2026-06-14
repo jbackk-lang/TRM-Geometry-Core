7fab5c22ae50285556bfd02235ebc97c037b7a37
Jacek Kielich <jbackk@gmail.com>
2026-06-14 10:52:45 +0200

    Add geometric conversion functions in transitions.py

---
+++ src/transitions.py
@@ -0,0 +1,10 @@
+import math
+
+def circle_to_triangle_operator():
+    return math.sqrt(3)
+
+def circle_to_square_operator():
+    return math.sqrt(math.pi)
+
+def sphere_to_cube_operator():
+    return 2 / math.sqrt(3)
